4d22b72a2ce5d915a53d0a17bf5fec7e6b786b73
Alessandro <alessandrorl@ufps.edu.co>
2024-12-02 11:54:10 -0500

    Modelo y cliente

---
+++ src/main/java/models/Cliente.java
@@ -0,0 +1,31 @@
+package com.example.facturacion.models;
+
+public class Cliente {
+    private String documento;
+    private String nombre;
+    private String tipoDocumento;
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+}
